077bb736742820bee4134ba5c2af8dc94afbf161
lazeicoder <anuragmukherjeedev@gmail.com>
2025-07-25 15:55:29 +0530

    Backend course codes added

---
+++ math.js
@@ -0,0 +1,18 @@
+exports.add = (a, b) => {
+    return a+b;
+};
+
+exports.sub = (a, b) => {
+    return a-b;
+}
+
+exports.multi = (a, b) => {
+    return a*b;
+}
+
+exports.divide = (a, b) => {
+    return a/b;
+}
+
+// module.exports = {add, sub, multi, divide};
+
---
+++ server.js
@@ -0,0 +1,10 @@
+
+const os = require('os');
+const path = require('path');
+const {add, sub, multi, divide} = require('./math');
+
+
+console.log(add(5, 19));
+console.log(multi(4, 8));
+console.log(sub(21, 3));
+console.log(divide(45, 2));
