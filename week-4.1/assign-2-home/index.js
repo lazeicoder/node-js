bb03371c11076ac5538b84da17776f38602035e5
lazeicoder <anuragmukherjeedev@gmail.com>
2025-07-27 20:14:21 +0530

    Added week-4 codes

---
+++ week-4.1/assign-2-home/index.js
@@ -0,0 +1,49 @@
+const { Command } = require('commander');
+const fs = require('fs').promises;
+const path = require('path');
+
+const program = new Command();
+
+// creating the json file
+const filePath = path.join(__dirname, 'files', 'tododata.json');
+
+const readTodos = async () => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+const writeTodo = async (todo) => {
+    await fs.writeFile(filePath, JSON.stringify(todo, null, 2));
+}
+
+program
+    .command('add <task>')
+    .description('Adds a new Todo')
+    .action(async (task) => {
+        const todo = await readTodos();
+        const nextId = todo.length > 0? todo[todo.length - 1].id + 1: 1;
+        const timeAdded = new Date().toLocaleString();
+        todo.push({id: nextId, task, timeAdded });
+        await writeTodo(todo);
+        console.log(`New todo has been added: ${task}`);
+    });
+
+program.command('list')
+    .description('Lists all the todos')
+    .action(async () => {
+        const todo = await readTodos();
+        if (todo.length === 0) {
+            console.log(`No todos available!! Add some.`);
+        }else {
+            console.log(`Your Todos:`);
+            todo.forEach( (doTodo) => {
+                console.log(`${doTodo.id} - ${doTodo.task}`);
+            });
+        }
+    });
+
+program.parse();
