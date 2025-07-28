b16033e0a4775c1e46449f7921a4c7c98ca419f9
lazeicoder <anuragmukherjeedev@gmail.com>
2025-07-29 00:47:26 +0530

    Added the module-2 codes

--- module-2/logEvents.js
+++ module-2/logEvents.js
@@ -7,11 +7,19 @@ const path = require('path');
 
 const logEvents = async (message) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
 
     try {
+        // if the directory doesn't exists then we create the directory
+
+        if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        }
+
+        // then we append into the text file => this is our event
         await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
+
     } catch (err) {
         console.log(err);
     }
