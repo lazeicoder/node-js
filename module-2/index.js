72916b8a36a61440e9523f23cd7019f18e1f7fb2
lazeicoder <anuragmukherjeedev@gmail.com>
2025-07-29 01:07:12 +0530

    Added the module-2 codes

--- module-2/index.js
+++ module-2/index.js
@@ -8,10 +8,14 @@ class MyEmitter extends EventEmitter {};
 const myEmitter = new MyEmitter();
 
 // add listener for the log event
+// So, we listen to an event using .on()
+
 myEmitter.on('log', (msg) => logEvents(msg));
 
+// We set a timer for about 2 seconds.
 setTimeout(() => {
     // Emit event
+    // We trigger an event using .emit() 
     myEmitter.emit('log', 'Log event emitted');
 
 }, 2000);
