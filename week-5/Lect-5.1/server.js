2518f241c46295abdc4a9a0743e593ec1af28772
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-10 13:56:36 +0530

    Adding week 5

--- week-5/Lect-5.1/server.js
+++ week-5/Lect-5.1/server.js
@@ -1,7 +1,20 @@
-async function fetchPost() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    const json = await res.json();
-    document.getElementById("posts").innerHTML = json.title;
+// Way 1: Simple fetch Data using js
+
+// async function fetchPost() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//     const json = await res.json();
+//     document.getElementById("posts").innerHTML = json.title;
+// }
+
+// fetchPost();
+
+// Way 2: Fetch data using axios
+
+async function getRecentPost() {
+    console.log(`Before sending request!`);
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+    document.getElementById("posts").innerHTML = response.data.title;
+    console.log(response.data.title);
 }
 
-fetchPost();
+getRecentPost();
