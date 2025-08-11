6d834c20e43b978a22e30575d48ef5f5099ac476
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-11 15:10:11 +0530

    practising random shit

--- week-5/index.js
+++ week-5/index.js
@@ -14,17 +14,35 @@ let timer = 0;
 const pizzaMaker = () => {
     if(timer === 0) {
         console.log(`Pizza making in progress!!`);
-    }else if (timer === 30) {
+    }else if (timer === 29) {
         console.log(`Grab a drink, your pizza is being servedd!!`);
     }
 }
 
-const progress = setInterval(() => {
+// const progress = setInterval(() => {
+//     pizzaMaker();
+//     timer++;
+//     if(timer === 29) {
+//         console.log(`Pizza is ready bitchh!!`);
+//     } else if (timer === 31) {
+//         clearInterval(progress);
+//     }
+// }, 1000);
+
+// More detailed process!!
+
+const progressTimer = setInterval(() => {
     pizzaMaker();
+    if (timer % 2 === 0) {
+        console.log(`Let him cook!! Time : ${timer}`);
+    }
+
     timer++;
-    if(timer === 29) {
+
+    if (timer === 30) {
         console.log(`Pizza is ready bitchh!!`);
     } else if (timer === 31) {
-        clearInterval(progress);
+        clearInterval(progressTimer);
+        console.log(`Your order has been delivered!!`);
     }
 }, 1000);
