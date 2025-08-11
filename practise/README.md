938bc66fabc0f53aa6f4c7a5e5fa0d3a76cbebdf
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-11 19:21:48 +0530

    practising random shit

---
+++ practise/README.md
@@ -0,0 +1,6 @@
+This is a readme file for just the Asynchronous JS concepts I have been going through.
+
+- Firstly `setTimeout()` is a functionality that is provided by the browser and is used in js scripts. It executes a particular block of code once after a specified time has elapsed.
+```
+setTimeout(callback, delay, param1, param2, ...)
+```
---
+++ practise/index.js
@@ -0,0 +1,31 @@
+// practising callbacks
+
+// Example-1
+// console.log(`Fetching data.....`);
+
+// function fetchData(callback) {
+//     setTimeout(() => {
+//         console.log(`Data fetched from server!!`);
+//         callback();
+//     }, 3000);
+// }
+
+// fetchData(() => {
+//     console.log(`Processing fetched data....`);
+// });
+
+// console.log(`Prepare other tasks while we fetch data!!`);
+
+// Example-2
+
+const greetPerson = (nameIs) => {
+    console.log(`Hello there, ${nameIs}`);
+}
+
+const greetName = (greetCallback) => {
+    const nameIs = `Sursadhu`;
+    console.log(`Calling name : ${nameIs}`);
+    greetCallback(nameIs);
+}
+
+greetName(greetPerson);
