b329a5d45cf88cafcf44557c7e14276eca5b52ea
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-11 12:44:54 +0530

    Added assignment

--- week-5/Assignment-Server/server.js
+++ week-5/Assignment-Server/server.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const app = express();
 
+app.get('/multiply', (req, res) => {
+    let { a, b } = req.query;
+    a = parseInt(a);
+    b = parseInt(b);
+
+    res.json({
+        multiply: a*b
+    });
+});
+
+app.get('/add', (req, res) => {
+    let { a, b } = req.query;
+    a = parseInt(a);
+    b = parseInt(b);
+
+    res.json({
+        add: a+b
+    });
+});
+
+app.get('/divide', (req, res) => {
+    let { a, b } = req.query;
+    a = parseInt(a);
+    b = parseInt(b);
+
+    res.json({
+        divide: a/b
+    });
+});
+
+app.get('/subtract', (req, res) => {
+    let { a, b } = req.query;
+    a = parseInt(a);
+    b = parseInt(b);
+
+    res.json({
+        subtract: a-b 
+    });
+});
+
 app.listen(3000, () => {
     console.log(`The server is running on port : 3000`);
 });
